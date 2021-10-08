1758f781fb696d511a68e26f6c01061675748fd0
Levi Aul <levi@leviaul.com>
2021-10-08 09:41:56 -0700

    Fix style complaints

--- Formula/openjdk-loom.rb
+++ Formula/openjdk-loom.rb
@@ -3,6 +3,9 @@ class OpenjdkLoom < Formula
   homepage "http://jdk.java.net/loom/"
   version "18.2.74"
 
+  license "GPL-2.0-only" => { with: "Classpath-exception-2.0" }
+  keg_only :shadowed_by_macos
+
   on_macos do
     if Hardware::CPU.arm?
       url "https://download.java.net/java/early_access/loom/2/openjdk-18-loom+2-74_macos-aarch64_bin.tar.gz", using: :nounzip
@@ -12,6 +15,7 @@ class OpenjdkLoom < Formula
       sha256 "8350da9ceb94beae89de4c7d124a2fad8e7dfc62ed39af8a36ca296bda5ae08f"
     end
   end
+
   on_linux do
     if Hardware::CPU.arm?
       url "https://download.java.net/java/early_access/loom/2/openjdk-18-loom+2-74_linux-aarch64_bin.tar.gz", using: :nounzip
@@ -20,13 +24,7 @@ class OpenjdkLoom < Formula
       url "https://download.java.net/java/early_access/loom/2/openjdk-18-loom+2-74_linux-x64_bin.tar.gz", using: :nounzip
       sha256 "7cee3ff133f6a15df3510dabf33871c206aa20a8e5cc08581f7281bf1a7835d0"
     end
-  end
 
-  license "GPL-2.0-only" => { with: "Classpath-exception-2.0" }
-
-  keg_only :shadowed_by_macos
-
-  on_linux do
     depends_on "alsa-lib"
     depends_on "cups"
     depends_on "fontconfig"
@@ -46,15 +44,15 @@ class OpenjdkLoom < Formula
   fails_with gcc: "5"
 
   def install
-    dist_tarball = Pathname.pwd.children.find{ |f| f.file? and f.basename.to_s =~ /\.tar\.gz$/ }
+    dist_tarball = Pathname.pwd.children.find { |f| f.file? && f.basename.to_s =~ /\.tar\.gz$/ }
     system "tar", "-x", "-f", dist_tarball.to_s
     dist_tarball.unlink
 
     unpacked_to_path =
       if OS.mac?
-        Pathname.pwd / 'jdk-18.jdk'
+        Pathname.pwd / "jdk-18.jdk"
       else
-        Pathname.pwd / 'jdk-18'
+        Pathname.pwd / "jdk-18"
       end
 
     libexec.install unpacked_to_path => "openjdk-loom.jdk"
@@ -63,16 +61,16 @@ class OpenjdkLoom < Formula
 
     jdk_root =
       if OS.mac?
-        jdk_installed / 'Contents' / 'Home'
+        jdk_installed / "Contents" / "Home"
       else
         jdk_installed
       end
 
     platform_name =
       if OS.mac?
-        'darwin'
+        "darwin"
       else
-        'linux'
+        "linux"
       end
 
     bin.install_symlink Dir[jdk_root / "bin/*"]
