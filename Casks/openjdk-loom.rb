da0d4a87e28b814ffd4047881ad9ffa4c17879b6
Levi Aul <levi@leviaul.com>
2021-11-17 12:27:07 -0800

    openjdk-loom 18.5.274

--- Casks/openjdk-loom.rb
+++ Casks/openjdk-loom.rb
@@ -2,16 +2,16 @@ cask "openjdk-loom" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
   version_major = 18
-  version_minor = 2
-  version_patch = 74
+  version_minor = 5
+  version_patch = 274
 
   version "#{version_major}.#{version_minor}.#{version_patch}"
 
   url "https://download.java.net/java/early_access/loom/#{version_minor}/openjdk-#{version_major}-loom+#{version_minor}-#{version_patch}_macos-#{arch}_bin.tar.gz"
   if Hardware::CPU.intel?
-    sha256 "8350da9ceb94beae89de4c7d124a2fad8e7dfc62ed39af8a36ca296bda5ae08f"
+    sha256 "98010b952880202736f7620d6eff4ee31ad338d63131e8f2e273082bcb386f2d"
   else
-    sha256 "9acbd6377f2a8452fc0e5f6874484173eeee63681dfa0f3affcfd3d13f1ee012"
+    sha256 "2f031d4938ed4b4b1f343559dc38b2ee010a6003c603579d2e063f1486b469a1"
   end
 
   name "OpenJDK Development Kit Project Loom EA Release"
