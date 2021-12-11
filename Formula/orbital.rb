f052382b5de8ae388137f4f85a953a1213a33e55
Levi Aul <levi@leviaul.com>
2021-12-10 19:51:10 -0800

    Orbital v1.0.11

--- Formula/orbital.rb
+++ Formula/orbital.rb
@@ -1,16 +1,10 @@
 class Orbital < Formula
   desc "Covalent ops tooling"
   homepage "https://github.com/covalenthq/orbital"
-  url "https://github.com/covalenthq/orbital/archive/refs/tags/v1.0.10.tar.gz"
-  sha256 "9cc28d7ad04bc151e7ca71150a0a5b1e9d809d40cba2edc77bf0360694537a8f"
+  url "https://github.com/covalenthq/orbital/archive/refs/tags/v1.0.11.tar.gz"
+  sha256 "894bf93ae880dddffc47c0ada1b287d35d8ce7a2b4ea3c499360ca3e604ef33c"
   license "MIT"
   revision 1
-
-  bottle do
-    root_url "https://github.com/covalenthq/homebrew-covalent/releases/download/orbital-1.0.10_1"
-    sha256 cellar: :any,                 catalina:     "8ab61bb642dcd602e6d26ba7cdca98698873504cacec5059fd38f9ff88078ee8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a8d1cd2f189f906422cbcd48ffb1c3d57ed8fa8c41d1e9860863975456f7e6d9"
-  end
 
   depends_on "ruby"
 
