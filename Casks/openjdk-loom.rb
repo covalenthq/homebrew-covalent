3e07b717aef38c32cf65c3ef4cd098b16540e829
Levi Aul <levi@leviaul.com>
2022-06-19 12:33:40 -0700

    openjdk-loom 19.6.625
    OpenJDK-Loom 19.6.625
    
    Closes #7.
    
    Signed-off-by: Levi Aul <levi@leviaul.com>

--- Casks/openjdk-loom.rb
+++ Casks/openjdk-loom.rb
@@ -1,18 +1,18 @@
 cask "openjdk-loom" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version_major = 18
+  version_major = 19
   version_minor = 6
-  version_patch = 282
+  version_patch = 625
 
   version "#{version_major}.#{version_minor}.#{version_patch}"
 
   url "https://pkgmirror.covalenthq.com/loom/openjdk-#{version_major}-loom+#{version_minor}-#{version_patch}_macos-#{arch}_bin.tar.gz",
       verified: "https://pkgmirror.covalenthq.com/loom"
   if Hardware::CPU.intel?
-    sha256 "cafc5626122a198445f372216a1bbf809ca01252a82e0787dd866130ab6d391c"
+    sha256 "c27180838a5491fe94b4f993da9523809031625b34e64c7338b3fc4c0758dd0b"
   else
-    sha256 "bfd6149e8cb5ba01816a73eabc15b62e45f5bdba41f41a635841aad5f2d1f764"
+    sha256 "c7711a2183ded8a543d36b16771febc01d2fc3c9f85432b597d6741911b96c16"
   end
 
   name "OpenJDK Development Kit Project Loom EA Release"
