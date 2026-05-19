03d321d7ded7e967fdbaaf6f086f93c2612aa09e
Levi Aul <levi@leviaul.com>
2026-05-19 18:45:11 +0000

    orbital: add 1.0.18 bottle.

--- Formula/orbital.rb
+++ Formula/orbital.rb
@@ -5,6 +5,12 @@ class Orbital < Formula
   sha256 "5a53bbaf57e16c4b6581edde886181fe362916cd4cecd0544aea6f042ceb5b56"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/covalenthq/homebrew-covalent/releases/download/orbital-1.0.18"
+    sha256 cellar: :any,                 arm64_tahoe:  "c67019d404373a0264a6412a43ef9c083afdd689de0f22eabd8e0c47cc256c3a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9324b6b6bf4bda1e9c554f00dd37623616afab4b6bf7a79d681c5db829ba2d8e"
+  end
+
   depends_on "ruby"
 
   def install
